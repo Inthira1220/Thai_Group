270c86c2f8a8cff32440633dc8f8938ac3aca864
Inthira K <kefind@gmail.com>
2021-06-04 23:41:55 -0400

    2nd committed

--- src/test/java/nadirProject3/project3vyTruck.java
+++ src/test/java/nadirProject3/project3vyTruck.java
@@ -1,4 +1,7 @@
 package nadirProject3;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -17,14 +20,26 @@ public class project3vyTruck {
        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
        driver.get("https://qa3.vytrack.com/user/login");
     }
-    @AfterClass
-    public void tearDown(){
-        driver.close();
-    }
-    @Test //(priority = 1)
+//    @AfterClass
+//    public void tearDown(){
+//        driver.close();
+//    }
+    @Test (priority = 1)
     public void logIn(){
+        WebElement loginVyTruck =driver.findElement(By.xpath("//input[@type='text']"));
+        loginVyTruck.sendKeys("user151");
+        WebElement passwordVyTruck =driver.findElement(By.xpath("//input[@type='password']"));
+        passwordVyTruck.sendKeys("UserUser123", Keys.ENTER);
 
     }
+    @Test (priority = 2)
+    public void fleetSelector (){
+        WebElement fleetSelectFirst = driver.findElement(By.xpath("//li[@class='dropdown dropdown-level-1']"));
+        fleetSelectFirst.click();
+        WebElement selectedFleet = driver.findElement(By.xpath("//span[@class='title title-level-2']"));
+        selectedFleet.click();
+    }
+
 
 
 }
