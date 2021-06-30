6af8324f307ec73ca08c215f397bc4a9e3c3ca1e
Inthira K <kefind@gmail.com>
2021-06-29 23:34:12 -0400

    test =4 committed

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,5 +2,9 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println();
 
+        //initial test 2 committed
+
+        System.out.println("initial test 2 committed");
+
     }
 }
